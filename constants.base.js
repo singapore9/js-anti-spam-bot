d071ceaf13ad92e32867f7c48052983052bd8efe
singapore9 <sharik.and@gmail.com>
2022-10-05 19:01:57 +0300

    constants

---
+++ constants.base.js
@@ -0,0 +1,10 @@
+const TELEGRAM_TOKEN = "";
+
+const FIREBASE_CERTIFICATE = {};
+const DATABASE_URL = "https://.firebasedatabase.app";
+
+module.exports = {
+    TELEGRAM_TOKEN,
+    FIREBASE_CERTIFICATE,
+    DATABASE_URL
+}
